009b5140140c089e394ee7cd19fa7e1ad65fb96d
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 <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>
2026-02-28 16:18:17 -0500

    .

--- include/wrapper.hpp
+++ include/wrapper.hpp
@@ -1,4 +1,4 @@
 #include "frechet.hpp"
 #include "curve.hpp"
 
-double compute_distance_parallel(const Curve&, const Curve&, bool forceOversize);
+double compute_distance_parallel(const Curve&, const Curve&, bool forceOversize = false);
